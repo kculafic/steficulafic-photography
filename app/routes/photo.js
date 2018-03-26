27c5a85c8102288243c9fbaaf31cc5fa672b849a
Krsto Culafic <kculafic@gmail.com>
2018-03-26 17:38:22 -0500

    Add vimeo embedded player to vimeo route

--- app/routes/photo.js
+++ app/routes/photo.js
@@ -7,15 +7,27 @@ export default Ember.Route.extend({
   return [{
       id: 1,
       link: 'https://i.imgur.com/sMTZRva.png',
-      fullRef: "<img src='https://i.imgur.com/sMTZRva.png' width='100%' height='100%'/>",
     }, {
       id: 2,
       link: 'https://i.imgur.com/u4g8tKK.png',
-      fullRef: "<img src='https://i.imgur.com/u4g8tKK.png' width='100%' height='100%'/>",
     }, {
       id: 3,
       link: 'https://i.imgur.com/eBCgsuO.png',
-      fullRef: "<img src='https://i.imgur.com/eBCgsuO.png' width='100%' height='100%'/>",
+    }, {
+      id: 4,
+      link: 'https://i.imgur.com/Cj9hwmP.png',
+    }, {
+      id: 5,
+      link: 'https://i.imgur.com/5kFLnK5.png',
+    }, {
+      id: 6,
+      link: 'https://i.imgur.com/1Btjrot.png',
+    }, {
+      id: 7,
+      link: 'https://i.imgur.com/x9xNpiM.png',
+    }, {
+      id: 8,
+      link: 'https://i.imgur.com/dOMVSVu.png',
     }];
   }
 });
