5b3a5a820b3d51723161583f009fd06aa704e094
Krsto Culafic <kculafic@gmail.com>
2018-03-26 17:10:31 -0500

    Hardcode some img links for photos into route

--- app/routes/photo.js
+++ app/routes/photo.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+  // return this.get('store').findAll('photo');
+  return [{
+      id: 1,
+      link: 'https://i.imgur.com/sMTZRva.png',
+      fullRef: "<img src='https://i.imgur.com/sMTZRva.png' width='100%' height='100%'/>",
+    }, {
+      id: 2,
+      link: 'https://i.imgur.com/u4g8tKK.png',
+      fullRef: "<img src='https://i.imgur.com/u4g8tKK.png' width='100%' height='100%'/>",
+    }, {
+      id: 3,
+      link: 'https://i.imgur.com/eBCgsuO.png',
+      fullRef: "<img src='https://i.imgur.com/eBCgsuO.png' width='100%' height='100%'/>",
+    }];
+  }
 });
