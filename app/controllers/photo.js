56fa49c8eb13ccce15550da0f2aa63071d4e0a6d
Krsto Culafic <kculafic@gmail.com>
2018-03-27 14:22:35 -0500

    Fill out photo collection

--- app/controllers/photo.js
+++ app/controllers/photo.js
@@ -10,6 +10,38 @@ export default Ember.Controller.extend({
     'https://i.imgur.com/1Btjrot.png',
     'https://i.imgur.com/x9xNpiM.png',
     'https://i.imgur.com/dOMVSVu.png',
+    'https://i.imgur.com/CxTJn27.png',
+    'https://i.imgur.com/oTNRUbq.png',
+    'https://i.imgur.com/ZMCfv1W.jpg',
+    'https://i.imgur.com/5OFZthk.png',
+    'https://i.imgur.com/9FARfu4.png',
+    'https://i.imgur.com/W85jP3A.png',
+    'https://i.imgur.com/A96GLt1.png',
+    'https://i.imgur.com/VpzW0uw.jpg',
+    'https://i.imgur.com/6Ax77sp.png',
+    'https://i.imgur.com/B22ExSd.jpg',
+    'https://i.imgur.com/5WVmrBf.png',
+    'https://i.imgur.com/ehbvbmU.png',
+    'https://i.imgur.com/6qDyJPR.jpg',
+    'https://i.imgur.com/VbOjWzy.png',
+    'https://i.imgur.com/ZHb5Zgj.png',
+    'https://i.imgur.com/CkpkSHq.png',
+    'https://i.imgur.com/Z9jJCxF.png',
+    'https://i.imgur.com/CJyzD2S.png',
+    'https://i.imgur.com/GhTfE8G.png',
+    'https://i.imgur.com/viOVYpp.png',
+    'https://i.imgur.com/sI1kND7.png',
+    'https://i.imgur.com/40Jm4XI.png',
+    'https://i.imgur.com/7cn2jQ5.png',
+    'https://i.imgur.com/y4qcph9.png',
+    'https://i.imgur.com/B3G47tq.png',
+    'https://i.imgur.com/GqJ5i8x.png',
+    'https://i.imgur.com/GQ1Orhf.png',
+    'https://i.imgur.com/1Xtw6ol.png',
+    'https://i.imgur.com/HHCvYsa.png',
+    'https://i.imgur.com/6FcQAQY.png',
+    'https://i.imgur.com/JvOccOE.jpg',
+    'https://i.imgur.com/Pkk9fvn.png'
   ],
 
   idCount: 0,
@@ -21,13 +53,13 @@ export default Ember.Controller.extend({
       this.idCount++;
       let x = this.idCount;
 
-      if (this.idCount <= 7) {
+      if (this.idCount <= 39) {
         this.set('photoRef', this.collection[x]);
       }
 
       else {
         this.set('idCount', 0);
-        this.set('photoRef', this.collection[0]) 
+        this.set('photoRef', this.collection[0]);
       }
     }
   }
